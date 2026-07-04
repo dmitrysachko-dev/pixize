307afcc091d279cb2a8c984c90e1bb6e92a36b7c
Dmitry Sachko <>
2026-07-04 20:54:51 +0300

    add deploy

--- vite.landing.config.ts
+++ vite.landing.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'node:path'
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === 'true' ? '/pixize/' : '/',
   root: resolve(__dirname, 'landing'),
   plugins: [vue()],
   css: {
